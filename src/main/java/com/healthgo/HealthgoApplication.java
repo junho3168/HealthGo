f0bf29e526593c5d9e187ec251bdfe90df77be44
junho3168 <junho3168@naver.com>
2025-07-13 12:27:08 +0900

    setting

---
+++ src/main/java/com/healthgo/HealthgoApplication.java
@@ -0,0 +1,13 @@
+package com.healthgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthgoApplication.class, args);
+	}
+
+}
