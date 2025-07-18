486928248dfa3e0dc1c1ce97d05f70ecd98df517
junho3168 <junho3168@naver.com>
2025-07-18 11:28:02 +0900

    feature - SecurityConfig 추가 및 회원가입과 로그인 권한허용

---
+++ src/main/java/com/healthgo/global/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.healthgo.global;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.securityMatcher("/**")
+			.csrf(csrf -> csrf.disable())
+			.authorizeHttpRequests(auth -> auth
+				.requestMatchers("/api/users/signup", "/api/users/login").permitAll()
+				.anyRequest().authenticated()
+			);
+		return http.build();
+	}
+}
